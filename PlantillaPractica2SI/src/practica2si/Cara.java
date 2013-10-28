9fc119217fb3ceb70cac4fbaecf502836f7c1410
Pablo <pablovargan@gmail.com>
2013-10-28 10:15:49 +0100

    primer commit

---
+++ PlantillaPractica2SI/src/practica2si/Cara.java
@@ -0,0 +1,56 @@
+package practica2si;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * Clase cara. Representa un ejemplo para el aprendizaje.
+ * @author dviejo
+ */
+public class Cara {
+	private int []data;
+	private int tipo;
+	
+        /**
+         * Lee la información de una imagen desde un fichero. Le asigna el tipo
+         * que se recibe como parametro
+         * @param fcara fichero de entrada
+         * @param tipo 1 = cara; -1 = nocara
+         */
+        public Cara(File fcara, int tipo)
+	{
+		int mask = 0x000000FF;
+		int cont;
+		BufferedImage bimage;
+		try
+		{
+			bimage = ImageIO.read(fcara);
+			data = bimage.getRGB(0, 0, bimage.getWidth(), bimage.getHeight(), null, 0, bimage.getWidth());
+			//Asumiendo que la imagen ya está en escala de grises pero en formato color, 
+			//convertimos ARGB en un único valor
+			for(cont=0;cont<data.length;cont++)
+				data[cont] = data[cont] & mask; 
+			this.tipo = tipo;
+		} catch (IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public int []getData()
+	{
+		return data;
+	}
+	
+	public int getTipo()
+	{
+		return tipo;
+	}
+	
+	public void setTipo(int newtipo)
+	{
+		tipo = newtipo;
+	}
+}
---
+++ PlantillaPractica2SI/src/practica2si/Practica2SI.java
@@ -0,0 +1,211 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2si;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author dviejo
+ */
+public class Practica2SI {
+
+    private String rutaDir;
+    private File []files;
+    private int NUM_ITERACIONES; 
+    private int NUM_CLASIFICADORES;
+    private boolean VERBOSE;
+
+    private double testRate;
+
+    private ArrayList<Cara> listaAprendizaje;
+    private ArrayList<Cara> listaTest;
+
+    public Practica2SI()
+    {
+        rutaDir = "";
+        testRate = 0.5;
+	NUM_ITERACIONES = 1;
+	NUM_CLASIFICADORES = 1;
+        VERBOSE = false;
+    }
+    
+    public void init()
+    {
+        int cont;
+	int aciertos, clase;
+	System.out.println("Sistemas Inteligentes. Segunda practica");
+		
+	getFileNames(rutaDir+"cara/");
+	listaAprendizaje = new ArrayList<Cara>();
+	for(cont=0;cont<files.length;cont++)
+	{
+		if(!files[cont].isDirectory())
+		{
+			listaAprendizaje.add(new Cara(files[cont],1));
+		}
+	}
+	getFileNames(rutaDir+"noCara/");
+	for(cont=0;cont<files.length;cont++)
+	{
+		if(!files[cont].isDirectory())
+		{
+			listaAprendizaje.add(new Cara(files[cont], -1));
+		}
+	}
+	System.out.println(listaAprendizaje.size()+ " imágenes encontradas");
+		
+	//inicializamos las listas
+	listaTest = new ArrayList<Cara>();
+		
+	//Separamos los conjuntos de aprendizaje y test
+	CrearConjuntoAprendizajeyTest();
+	System.out.println(listaAprendizaje.size()+" imagenes para aprendizaje, "+listaTest.size()+" imagenes para el test ("+(testRate*100)+"%)");
+
+	//Comenzamos el aprendizaje
+	long t1 = System.currentTimeMillis();
+        //TODO Aquí debéis poner vuestra llamada al método de entrenamiento de AdaBoost
+	long t2 = System.currentTimeMillis();
+	long time;
+        
+	time = t2 - t1;
+	System.out.println("Tiempo empleado en el aprendizaje: "+((float)time/1000f)+" segundos");
+	System.out.println("Número de clasificadores encontrados: "); //TODO añadir el valor
+	//Test final
+        if(VERBOSE)
+        {
+            aciertos = 0;
+            for(Cara c: listaAprendizaje)
+            {
+                clase = -1;  //TODO Cambiar -1 por la llamada a clasificar utilizando el clasificador fuerte
+                        //de Adaboost para el ejemplo c
+                if(clase == c.getTipo())
+                    aciertos++;
+            }
+            System.out.println("APRENDIZAJE. Tasa de aciertos: "+((float)aciertos/(float)(listaAprendizaje.size())*100.0f)+"%");
+        }
+        
+	//Comprobamos el conjunto de test
+	aciertos = 0;
+	for(Cara c: listaTest)
+	{
+		clase = -1;  //TODO Cambiar -1 por la llamada a clasificar utilizando el clasificador fuerte
+                            //de Adaboost para el ejemplo c
+		if(clase == c.getTipo())
+			aciertos++;
+	}
+	System.out.println("TEST. Tasa de aciertos: "+((float)aciertos/(float)(listaAprendizaje.size())*100.0f)+"%");
+
+    }
+    
+    /**
+     * Selecciona al azar un subconjunto para Test. El resto compondrá el conjunto de aprendizaje
+     */
+    private void CrearConjuntoAprendizajeyTest()
+    {
+    	int totalTam = listaAprendizaje.size();
+	int tamTest = (int)(totalTam * testRate);
+	int cont;
+	Random rnd = new Random(System.currentTimeMillis());
+		
+	for(cont=0;cont<tamTest;cont++)
+	{
+            listaTest.add(listaAprendizaje.remove(rnd.nextInt(totalTam)));
+            totalTam--;
+	}
+    }
+    
+    public void getFileNames(String ruta)
+    {
+    	File directorio = new File(ruta);
+	if (!directorio.isDirectory())
+            throw new RuntimeException("La ruta debe ser un directorio");
+	ImageFilter filtro = new ImageFilter();
+	files = directorio.listFiles(filtro);
+    }
+
+	public void setRuta(String r)
+	{
+		rutaDir = r;
+	}
+	
+	public void setRate(double t)
+	{
+		testRate = t;
+	}
+	
+	public void setNumIteraciones(int t)
+	{
+		NUM_ITERACIONES = t;
+	}
+	public void setNumClasificadores(int c)
+	{
+		NUM_CLASIFICADORES = c;
+	}
+
+	public void setVerbose(boolean v)
+	{
+		VERBOSE = v;
+	}
+	
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+		int cont;
+		Practica2SI programa;
+		String option;
+		boolean maluso = true;
+		int paso = 2;
+		
+		programa = new Practica2SI();
+
+		for(cont = 0; cont < args.length; cont+=paso)
+		{
+			option = args[cont];
+			if(option.charAt(0) == '-')
+			{
+				switch(option.charAt(1))
+				{
+				case 'd':
+					programa.setRuta(args[cont+1]);
+					paso = 2;
+                                        maluso = false;
+					break;
+				case 't':
+					programa.setRate(Double.parseDouble(args[cont+1]));
+					paso = 2;
+					break;
+				case 'T':
+					programa.setNumIteraciones(Integer.parseInt(args[cont + 1]));
+					paso = 2;
+					break;
+				case 'c':
+					programa.setNumClasificadores(Integer.parseInt(args[cont + 1]));
+					paso = 2;
+					break;
+				case 'v':
+					programa.setVerbose(true);
+					paso = 1;
+					break;
+				default:
+					maluso = true;
+				}
+			}
+			else maluso = true;
+		}
+		
+		if(!maluso)
+			programa.init();
+		else
+		{
+			System.out.println("Lista de parametros incorrecta");
+			System.out.println("Uso: java Practica2SI -d ruta [-t testrate] [-T maxT] [-c numClasificadores] [-v]");
+		}
+    }
+}
